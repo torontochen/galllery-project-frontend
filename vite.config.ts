13e2ad26ef879e0808ca5c7326eb61bfaa7743b6
Jack <chenhaoyujc@gmail.com>
2026-02-07 17:13:55 -0500

    install tailwind and modify vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  console.log(env.VITE_DEBUG);
+
+  return {
+    plugins: [react()],
+    server: {
+      ...(env.VITE_DEBUG === "true" && {
+        proxy: {
+          "/api": {
+            target: "http://localhost:8000",
+            changeOrigin: true,
+            secure: false,
+          },
+        },
+      }),
+    },
+  };
+});
